12becd841e02f2648b7f2b29d3d5d2a0651edf5d
Sylvia <sylvia@donchev.is>
2022-04-23 21:58:11 +0200

    variable list created

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
-let input = $('input');
-inputs.addClass('my-class');
+
+// adding new item to the list of items
+function newItem()
+let li = document.createElement("li");
+let inputValue = document.getElementById("input").value;
+let text = document.createTextNode(inputValue);
+li.appendChild(text);
+
+if (inputValue === '' ) {
+alert("You must write something !");
+} else {
+  let list = document.querySelector('#list'); //list is an id in the form in the HTML
+  list.appendChild(li);
+}

734f2969b810fe9b6adec5436d3a423cbe04bd7a
Sylvia <sylvia@donchev.is>
2022-04-23 22:11:07 +0200

    crossOut fn, addEventList. x-ing out an li of toDo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-// adding new item to the list of items
+//1. Adding new item to the list of items
 function newItem()
 let li = document.createElement("li");
 let inputValue = document.getElementById("input").value;
@@ -12,3 +12,13 @@ alert("You must write something !");
   let list = document.querySelector('#list'); //list is an id in the form in the HTML
   list.appendChild(li);
 }
+
+ //2. Crossing out an item from the list of items:
+
+ function crossOut() {
+   li.classList.toggle("strike"); //Toggling:means if there is no class name assigned to the element,
+   // then a class name can be assigned to it dynamically or if a certain class is already present,
+   // then it can be removed dynamically by just using the toggle()
+ }
+
+ li.addEventListener("click",crossOut);
